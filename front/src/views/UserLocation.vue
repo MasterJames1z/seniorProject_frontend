5a4cfa9fb24a1641b97f82a0d0cfc0f6eb7fb0cd
worawut aunkham <jameskater350@gmail.com>
2023-07-07 15:30:47 +0700

    Try to create 2 field for create distance matrix

--- front/src/views/UserLocation.vue
+++ front/src/views/UserLocation.vue
@@ -1,24 +1,60 @@
 <template>
-  <GoogleMap
-    api-key="AIzaSyAAUnokPnN8yWpQqaf5rFPIWrqyM26f1E4"
-    style="width: 100%; height: 500px"
-    :center="center"
-    :zoom="15"
-  >
-    <Marker :options="{ position: center }" />
-  </GoogleMap>
+  <div class="flex flex-col items-center mt-8">
+    <label class="text-gray-700 mb-2">Origin:</label>
+    <input
+      v-model="origin"
+      type="text"
+      class="w-64 px-4 py-2 border border-gray-300 rounded-md"
+    />
+
+    <label class="text-gray-700 mt-4 mb-2">Destination:</label>
+    <input
+      v-model="destination"
+      type="text"
+      class="w-64 px-4 py-2 border border-gray-300 rounded-md"
+    />
+
+    <button
+      @click="calculateButtonPressed"
+      class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Calculate
+    </button>
+  </div>
 </template>
 
 <script>
-// import { defineComponent } from "vue";
-import { GoogleMap, Marker } from "vue3-google-map";
+import axios from "axios";
 
 export default {
-  components: { GoogleMap, Marker },
-  setup() {
-    const center = { lat: 18.800485862344402, lng: 98.95041157342138 };
-
-    return { center };
+  data() {
+    return {
+      origin: { lat: null, lng: null },
+      destination: { lat: null, lng: null },
+    };
+  },
+  methods: {
+    calculateButtonPressed() {
+      // const URL =
+      //   "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?origins=${this.route.origin.lat},${this.route.origin.lng}&destinations=${this.route.destination.lat},${this.route.destination.lng}&key=AIzaSyAAUnokPnN8yWpQqaf5rFPIWrqyM26f1E4";
+      // const URL =
+      //   "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?origins=40.6655101%2C-73.89188969999998&destinations=40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626&key=AIzaSyAAUnokPnN8yWpQqaf5rFPIWrqyM26f1E4";
+      axios
+        .get(URL)
+        .then((response) => {
+          console.log("Done");
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+      console.log("Origin:", this.origin);
+      console.log("Destination:", this.destination);
+    },
   },
 };
 </script>
+
+<style scoped>
+/* Add Tailwind CSS classes here */
+</style>
