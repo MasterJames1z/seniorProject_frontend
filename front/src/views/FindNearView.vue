6401fda369d89fa5646693504c5ddb0294e17cbf
worawut aunkham <jameskater350@gmail.com>
2023-07-08 22:14:58 +0700

    Group search by origin to findnear page

--- front/src/views/FindNearView.vue
+++ front/src/views/FindNearView.vue
@@ -1,80 +1,81 @@
 <template>
-  <div class="grid grid-cols-2">
+  <div class="grid grid-cols-1 sm:grid-cols-2">
     <div class="w-full">
       <h1>Address</h1>
-      <form class="bg-white rounded-lg shadow-lg p-6">
-        <div class="mb-4">
-          <div class="flex">
+      <form class="bg-white rounded-lg shadow-lg p-6" @submit="handleSubmit">
+        <div class="flex items-center mb-4">
+          <input type="checkbox" class="mr-2" v-model="showTemplates" />
+          <label class="text-gray-700">Find by Origin&Destination</label>
+        </div>
+
+        <!-- Template 1 -->
+        <label for="currentLocationInput">Current location:</label>
+        <div class="flex">
+          <i class="fas fa-dot-circle fa-lg text-indigo-500 cursor-pointer">
+            <svg
+              class="h-8 w-8 text-red-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              @click="locatorButtonPressed"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+          </i>
+          <input
+            type="text"
+            placeholder="Enter your address"
+            class="w-full border-2 border-gray-300 focus:outline-none focus:border-indigo-500 rounded-lg px-4 py-2"
+            v-model="currentLocationText"
+          />
+          <!-- <button
+            class="bg-indigo-500 text-white rounded-lg px-4 py-2"
+            @click="findCloseBuyButtonPressed"
+          >
+            Find Chager
+          </button> -->
+        </div>
+        <!-- Template 2 -->
+        <div v-if="showTemplates">
+          <div class="flex flex-col items-center mt-8">
+            <label class="text-gray-700 mb-2">Origin:</label>
             <input
+              v-model="origin"
               type="text"
-              placeholder="Enter your address"
-              class="w-full border-2 border-gray-300 focus:outline-none focus:border-indigo-500 rounded-lg px-4 py-2"
-              v-model="currentLocationText"
+              class="w-64 px-4 py-2 border border-gray-300 rounded-md"
             />
-            <i class="fas fa-dot-circle fa-lg text-indigo-500 cursor-pointer">
-              <svg
-                class="h-8 w-8 text-red-500"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                @click="locatorButtonPressed"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
-            </i>
+            <label class="text-gray-700 mt-4 mb-2">Destination:</label>
+            <input
+              v-model="destination"
+              type="text"
+              class="w-64 px-4 py-2 border border-gray-300 rounded-md"
+            />
+
+            <button
+              @click="calculateButtonPressed"
+              class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Calculate
+            </button>
+          </div>
+          <!-- Results section -->
+          <div v-if="distance" class="mt-4">
+            <p class="text-gray-700">Distance: {{ distance }}</p>
+            <p class="text-gray-700">Duration: {{ duration }}</p>
           </div>
         </div>
-        <!-- <div class="mb-4">
-          <div class="flex space-x-4">
-            <div class="w-1/2">
-              <select
-                v-model="type"
-                class="w-full border-2 border-gray-300 focus:outline-none focus:border-indigo-500 rounded-lg px-4 py-2"
-              >
-                <option value="gas_station">Electric station</option>
-              </select>
-            </div>
-            <div class="w-1/2">
-              <select
-                v-model="radius"
-                class="w-full border-2 border-gray-300 focus:outline-none focus:border-indigo-500 rounded-lg px-4 py-2"
-              >
-                <option value="5000">5 KM</option>
-                <option value="10000">10 KM</option>
-                <option value="15000">15 KM</option>
-                <option value="20000">20 KM</option>
-              </select>
-            </div>
-          </div>
-        </div> -->
-        <button
-          class="bg-indigo-500 text-white rounded-lg px-4 py-2"
-          @click="findCloseBuyButtonPressed"
-        >
-          Find Chager
-        </button>
       </form>
-      <!-- <div class="segment" style="max-height: 500px; overflow-y: scroll">
-        <div class="divide-y divide-gray-200">
-          <div class="item" v-for="place in places" :key="place.id">
-            <div class="content">
-              <div class="font-bold">{{ place.name }}</div>
-              <div class="text-gray-500">{{ place.vicinity }}</div>
-            </div>
-          </div>
-        </div>
-      </div> -->
     </div>
     <div>
       <h1>Maps</h1>
@@ -91,7 +92,7 @@
 </template>
 
 <script>
-// import axios from "axios";
+import axios from "axios";
 import { GoogleMap, Marker } from "vue3-google-map";
 
 export default {
@@ -101,9 +102,14 @@ export default {
       lng: null,
       type: "",
       radius: "",
+      origin: "",
+      destination: "",
+      distance: null,
+      duration: null,
       places: [],
       currentLocation: { lat: null, lng: null },
       currentLocationText: "",
+      showTemplates: false,
     };
   },
   components: { GoogleMap, Marker },
@@ -127,6 +133,28 @@ export default {
         }
       );
     },
+    calculateButtonPressed() {
+      const apiKey = "AIzaSyAAUnokPnN8yWpQqaf5rFPIWrqyM26f1E4 ";
+      const origin = encodeURIComponent(this.origin);
+      const destination = encodeURIComponent(this.destination);
+      const URL = `http://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${destination}&key=${apiKey}`;
+
+      axios
+        .get(URL)
+        .then((response) => {
+          const result = response.data.rows[0].elements[0];
+          this.distance = result.distance.text;
+          this.duration = result.duration.text;
+          console.log("Distance:", this.distance);
+          console.log("Duration:", this.duration);
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    },
+    handleSubmit(event) {
+      event.preventDefault();
+    },
   },
   watch: {
     currentLocation: {
@@ -138,24 +166,5 @@ export default {
       immediate: true,
     },
   },
-
-  // findCloseBuyButtonPressed() {
-  //   // const URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${this.lat},${this.lng}&type=${this.type}&radius=${this.radius}&key=AIzaSyAAUnokPnN8yWpQqaf5rFPIWrqyM26f1E4`;
-  //   // axios
-  //   //   .get(URL)
-  //   //   .then((response) => {
-  //   //     this.places = response.data.results;
-  //   //     // this.addLocationsToGoogleMaps();
-  //   //   })
-  //   //   .catch((error) => {
-  //   //     console.log(error.message);
-  //   //   });
-  //   // const current = this.locatorButtonPressed();
-  //   // console.log(this.lat, this.lng);
-  //   // return current;
-  //   const center = { lat: 18.800485862344402, lng: 98.95041157342138 };
-
-  //   return { center };
-  // },
 };
 </script>
