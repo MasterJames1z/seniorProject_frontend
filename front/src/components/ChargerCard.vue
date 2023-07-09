567f5a3e5485048029dbe3b711a65393cf0b93c5
worawut aunkham <jameskater350@gmail.com>
2023-07-09 20:39:55 +0700

    Add page to showing list of charger

---
+++ front/src/components/ChargerCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mx-auto px-4">
+    <div
+      class="h-full p-5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+    >
+      <a href="#">
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          {{ charger.Station_Name }}
+        </h5>
+      </a>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        {{ charger.Station_At }}
+      </p>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        Lat: {{ charger.lat }} || Lan: {{ charger.lon }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChargerCard",
+  props: {
+    charger: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- front/src/views/AboutView.vue
+++ front/src/views/AboutView.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h1>List of charger station in Chiang Mai</h1>
+  <chargerView
+    v-for="detail in myJson"
+    :key="detail.id"
+    :charger="detail"
+  ></chargerView>
 </template>
+
+<script>
+import json from "D:/Senior/seniorProject_frontend/front/db.json";
+import chargerView from "../components/ChargerCard.vue";
+
+export default {
+  components: {
+    chargerView,
+  },
+  data() {
+    return {
+      myJson: json,
+    };
+  },
+};
+</script>
