533752533365ba7078614aa25a9b6b3733b82117
worawut aunkham <jameskater350@gmail.com>
2023-07-17 15:08:34 +0700

    Add condition get login show logout button

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -1,11 +1,28 @@
 <template>
-  <nav class="bg-white dark:bg-gray-900 w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <nav
+    class="bg-white dark:bg-gray-900 w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600"
+  >
+    <div
+      class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+    >
       <a href="/" class="flex items-center">
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">EV Better</span>
+        <span
+          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+        >
+          EV Better
+        </span>
       </a>
       <div class="flex md:order-2">
         <button
+          v-if="currentUser"
+          @click="logout"
+          type="button"
+          class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+        >
+          Logout
+        </button>
+        <button
+          v-else
           @click="getLogin"
           type="button"
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -48,6 +65,7 @@
           <li>
             <a
               href="/"
+              v-if="currentUser"
               class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
               aria-current="page"
               >Home</a
@@ -56,6 +74,7 @@
           <li>
             <a
               href="/userlocation"
+              v-if="currentUser"
               class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >List Charger</a
             >
@@ -63,6 +82,7 @@
           <li>
             <a
               href="/findnear"
+              v-if="currentUser"
               class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >Find Charger</a
             >
@@ -70,6 +90,7 @@
           <li>
             <a
               href="/register"
+              v-if="currentUser"
               class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >Register</a
             >
@@ -83,9 +104,21 @@
 <script>
 export default {
   name: "NavBarComponent",
+  inject: ["GStore"],
+  computed: {
+    currentUser() {
+      return this.GStore.currentUser;
+    },
+  },
   methods: {
     getLogin() {
       this.$router.push("/login");
+    },
+    logout() {
+      this.GStore.currentUser = null;
+      // Perform any necessary logout operations
+      // Redirect to the desired route
+      // this.$router.push("/login");
     },
     toggleNavbar() {
       const navbarSticky = document.getElementById("navbar-sticky");
