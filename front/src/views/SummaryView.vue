788979cfe5844e9050052cf7d73bd7166dd56c63
worawut aunkham <jameskater350@gmail.com>
2023-10-08 23:10:28 +0700

    Add button to share web page

--- front/src/views/SummaryView.vue
+++ front/src/views/SummaryView.vue
@@ -1,18 +1,34 @@
 <template>
-  <!-- {{ totalCost }} -->
-  <!-- <h1>Summary of plan</h1> -->
   <div
+    class="bg-cover bg-center bg-no-repeat app"
     style="
       background-image: url('https://static.vecteezy.com/system/resources/previews/008/017/890/original/or-illustrator-of-yellow-electric-vehicle-with-an-electric-charging-cabinet-with-nature-green-grass-and-shadow-of-town-under-blue-sky-and-white-clouds-vector.jpg');
     "
-    class="app bg-cover"
   >
-    <BarChart />
+    <div class="container mx-auto p-8 text-center text-gray-700">
+      <BarChart />
+      <div class="mt-8">
+        <button
+          class="bg-green-500 hover:bg-green-700 text-white px-4 py-2 rounded mr-4"
+          @click="captureAndSaveWebpage"
+        >
+          Capture and Save Webpage
+        </button>
+        <button
+          class="bg-amber-500 hover:bg-amber-700 text-white px-4 py-2 rounded"
+          @click="sharePage"
+        >
+          Share This Page
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BarChart from "../components/BarChart.vue";
+import html2canvas from "html2canvas";
+import { saveAs } from "file-saver";
 
 export default {
   inject: ["GStore"],
@@ -24,15 +40,63 @@ export default {
       totalCost: this.GStore.totalCost,
     };
   },
+  methods: {
+    async captureAndSaveWebpage() {
+      // Capture the webpage content as an image using html2canvas
+      const webpageElement = document.querySelector(".chart-container");
+      const canvas = await html2canvas(webpageElement);
+
+      // Convert the captured image to a Blob
+      canvas.toBlob((blob) => {
+        // Save the Blob as a file
+        saveAs(blob, "webpage_capture.png");
+      });
+    },
+    sharePage() {
+      if (navigator.share) {
+        navigator
+          .share({
+            title: "EV Better summary",
+            text: "Check out this awesome page!",
+            url: "http://localhost:8080/summary",
+          })
+          .then(() => console.log("Shared successfully"))
+          .catch((error) => console.error("Error sharing:", error));
+      } else {
+        alert("Sharing not supported in this browser.");
+      }
+    },
+  },
 };
 </script>
 
 <style>
 .app {
+  min-height: 100vh;
+  background-color: rgba(
+    0,
+    0,
+    0,
+    0.6
+  ); /* Overlay with a semi-transparent background color */
+}
+
+/* Style the container and text */
+.container {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  min-height: 100vh;
-  background-color: #f3f4f6;
+  height: 100%;
+  /* text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); Add a subtle text shadow */
+}
+
+/* Style the buttons */
+button {
+  transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: #1e40af; /* Change button color on hover */
 }
 </style>
